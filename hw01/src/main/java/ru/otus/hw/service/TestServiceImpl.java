141b5ef7d68debe8d27117a67c0232b2b768a496
Dima Rogov <ds.rogv@gmail.com>
2025-08-16 12:52:23 +0300

    Output questions and answers

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,40 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao questionDao;
+
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+        var questions = questionDao.findAll();
+        printQuestions(questions);
+    }
+
+    private void printQuestions(List<Question> questions) {
+        for (int i = 0; i < questions.size(); ++i) {
+            var question = questions.get(i);
+            ioService.printFormattedLine("Question %d: %s", i + 1, question.text());
+            printAnswers(question.answers());
+            ioService.printLine("");
+        }
+    }
+
+    private void printAnswers(List<Answer> answers) {
+        for (int j = 0; j < answers.size(); ++j) {
+            var answer = answers.get(j);
+            ioService.printFormattedLine("%d) %s", j + 1, answer.text());
+        }
     }
 }
