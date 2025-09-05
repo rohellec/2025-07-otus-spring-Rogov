1068307bbed61cbe0a7b726e7a9c0abe3f4934dd
Dima Rogov <ds.rogv@gmail.com>
2025-09-05 21:56:26 +0300

    Move CsvQuestionDao from previous homework

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,25 +1,59 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
-@Component
 public class CsvQuestionDao implements QuestionDao {
+
+    private static final char CSV_SEPARATOR = ';';
+
+    private static final char SKIP_LINES_COUNT = 1;
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        var fileName = fileNameProvider.getTestFileName();
+        var inputStream = getFileFromResourceAsStream(fileName);
+        var reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            var csvToBean = buildCsvToBeanParser(reader);
+            return csvToBean.parse().stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (RuntimeException ex) {
+            throw new QuestionReadException("Error during parsing questions csv file " + fileName, ex);
+        }
+    }
 
-        return new ArrayList<>();
+    private CsvToBean<QuestionDto> buildCsvToBeanParser(Reader reader) {
+        return new CsvToBeanBuilder<QuestionDto>(reader)
+                .withType(QuestionDto.class)
+                .withSeparator(CSV_SEPARATOR)
+                .withSkipLines(SKIP_LINES_COUNT)
+                .build();
+    }
+
+    private InputStream getFileFromResourceAsStream(String fileName) {
+        var classLoader = getClass().getClassLoader();
+        var inputStream = classLoader.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new QuestionReadException("Questions file not found! " + fileName);
+        }
+        return inputStream;
     }
 }
