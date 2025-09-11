3fcee857eb465d18425e131aaa2998ef87bd7a91
Dima Rogov <ds.rogv@gmail.com>
2025-09-11 12:07:49 +0300

    Implement the questionnaire business logic using localized messages

--- hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,7 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -23,11 +24,29 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (int i = 0; i < questions.size(); ++i) {
+            var question = questions.get(i);
+            printQuestionWithAnswers(question, i + 1);
+
+            var answers = question.answers();
+            var appliedAnswer = ioService.readIntForRangeWithPromptLocalized(1, answers.size(),
+                    "TestService.answer.input.number",
+                    "TestService.answer.input.number.error");
+            ioService.printLine("");
+
+            var isAnswerValid = answers.get(appliedAnswer - 1).isCorrect(); // Задать вопрос, получить ответ
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
     }
 
+    private void printQuestionWithAnswers(Question question, int order) {
+        ioService.printFormattedLineLocalized("TestService.question.number", order, question.text());
+        var answers = question.answers();
+
+        for (int i = 0; i < answers.size(); ++i) {
+            var answer = answers.get(i);
+            ioService.printFormattedLine("%d) %s", i + 1, answer.text());
+        }
+    }
 }
