96a9c8a76cb7705fd1124bf6367393bf26c2c682
Dima Rogov <ds.rogv@gmail.com>
2025-10-01 19:27:50 +0300

    Add shell commands for authors

--- hw05/src/main/java/ru/otus/hw/commands/AuthorCommands.java
+++ hw05/src/main/java/ru/otus/hw/commands/AuthorCommands.java
@@ -22,4 +22,31 @@ public class AuthorCommands {
                 .map(authorConverter::authorToString)
                 .collect(Collectors.joining("," + System.lineSeparator()));
     }
+
+    @ShellMethod(value = "Find author by id", key = "abid")
+    public String findAuthorById(long id) {
+        return authorService.findById(id)
+                .map(authorConverter::authorToString)
+                .orElse("Book with id %d not found".formatted(id));
+    }
+
+    // ains fullName
+    @ShellMethod(value = "Insert author", key = "ains")
+    public String insertAuthor(String fullName) {
+        var savedAuthor = authorService.insert(fullName);
+        return authorConverter.authorToString(savedAuthor);
+    }
+
+    // aupd 1 newName
+    @ShellMethod(value = "Update author", key = "aupd")
+    public String updateAuthor(long id, String fullName) {
+        var savedAuthor = authorService.update(id, fullName);
+        return authorConverter.authorToString(savedAuthor);
+    }
+
+    // adel 1
+    @ShellMethod(value = "Delete author by id", key = "adel")
+    public void deleteAuthor(long id) {
+        authorService.deleteById(id);
+    }
 }
--- hw05/src/main/java/ru/otus/hw/services/AuthorService.java
+++ hw05/src/main/java/ru/otus/hw/services/AuthorService.java
@@ -3,7 +3,16 @@ package ru.otus.hw.services;
 import ru.otus.hw.models.Author;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
+    Optional<Author> findById(long id);
+
     List<Author> findAll();
+
+    Author insert(String fullName);
+
+    Author update(long id, String fullName);
+
+    void deleteById(long id);
 }
